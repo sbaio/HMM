34d61550abdeb56d429edc84aad02c213288e32d
chaimaa kadaoui <chaimaa.kadaoui@gmail.com>
2016-12-19 19:44:56 +0100

    Debugging of EM... Now it works

--- HMM_Othman.py
+++ HMM_Othman.py
@@ -3,7 +3,6 @@
 import numpy as np
 from sklearn import preprocessing
 from scipy.stats import multivariate_normal
-from scipy.misc import logsumexp
 import em_gauss
 
 import matplotlib.pyplot as plt
@@ -17,97 +16,90 @@ def sum_lse(x):
     return np.exp(np.max(a) + np.log(np.sum(np.exp(a-np.max(a)))))
 
 ## alpha recursion
-# Name : log alpha
-# Input : U the data, A the transition matrix, pi the initial probability distribution, mu & sigma the parameters of the normal
-# Output: The matrix of the log alpha
-
+# A is the transition matrix
+# pi the initial probability distribution
+# mu & sigma the parameters of the normal
 # mu is a matrix of K rows and 2 columns
 # sigma is a list of K covariance matrices
-
-def logalpha(U, A, pi, mu, sigma):
-    n = len(U) # number of rows , data points
+# compute_logalpha returns a (T,K) matrix
+def compute_logalpha(U, A, pi, mu, sigma):
+    (T,d) = U.shape
     K = len(pi) # number of classes
-    logalpha = np.zeros((n,K))
+    logalpha = np.zeros((T,K))
 
     # first alpha is computed with initial proba distribution
     for k in range(0,K):
         mvnU = multivariate_normal.pdf(U[0,:], mean=mu[k,:], cov=sigma[k])
         logalpha[0,k] = np.log(pi[k]) + np.log(mvnU)
 
-    for t in range(1,n):
+    for t in range(1,T):
         for k in range(0,K):
             # We create for each k the vector a such that:
             # a[i]=log(p(qt=k|q(t-1)=i))+log(alpha(q(t-1)=i))
             # k corresponds to the current state (t)
             # i corresponds to the previous state (t-1)
-
             a = logalpha[t-1,:] + np.log(A[k,:])
-
             mvnU = multivariate_normal.pdf(U[t,:], mean=mu[k,:], cov=sigma[k])
             logalpha[t,k] = np.log(mvnU) + np.max(a) + logsumexp(a-np.max(a))
+
     return logalpha
 
-
-def logbeta(U,A,pi,mu,sigma):
-    n = len(U) # number of rows , data points
+## beta recursion
+# compute_logbeta returns a (T,K) matrix
+def compute_logbeta(U, A, pi, mu, sigma):
+    (T,d) = U.shape
     K = len(pi) # number of classes
-    logbeta = np.zeros((n,K))
+    logbeta = np.zeros((T,K))
 
     # The last beta is given by the vector (1,1,1,1)
-    logbeta[n-1,:] = np.log(np.ones((1,K)))
+    logbeta[T-1,:] = np.log(np.ones((1,K)))
 
     # The other are computed with the recursive formula
     # We fill our matrix logbeta row by row
-    for t in range(n-2,-1,-1):
+    for t in range(T-2,-1,-1):
         for k in range(0,K):
             # We create for each k the vector a such that:
             # a[i]=log(beta(q(t+1)=i))+log(p(q(t+1)=i|q(t)=k))+log(p(u(t+1)|q(t+1)=i))
             # k corresponds to the current state q(t)
             # i corresponds to the following state q(t+1)
-
             a = np.zeros(K)
             for i in range(0,K):
                 mvnU = multivariate_normal.pdf(U[t+1,:], mean=mu[i,:], cov=sigma[i])
                 a[i] = logbeta[t+1,i] + np.log(A[i,k]) + np.log(mvnU)
-
             logbeta[t,k] = np.max(a) + logsumexp(a-np.max(a))
+
     return logbeta
 
+## computes log p(u_1, ..., u_T)
+def compute_log_likelihood(logbeta, logalpha):
+    # We take for example q_0 to compute the probability by marginalization
+    a = (logalpha + logbeta)[0, :]
+    log_likelihood = np.max(a) + logsumexp(a - np.max(a))
+    return log_likelihood
 
-##  Smoothing p(qt|u1,...,uT) returns matrix of size T x K .. proba that ut belongs to class k, given all observations
-def smoothing(beta, alpha):
-
-    log_nom = beta + alpha
-    (n,K) = log_nom.shape
-    a = log_nom-np.max(log_nom,axis=1).reshape((n,1))
-    log_denom = np.max(log_nom,axis=1) + np.log(np.sum(np.exp(a),axis=1))
-
-    return np.exp(log_nom-log_denom.reshape((n,1)))
+## Smoothing p(q_t|u_1,...,u_T) returns matrix of size T x K
+## proba that u_t belongs to class k, given all observations
+def smoothing(logbeta, logalpha):
+    log_nom = logbeta + logalpha
+    log_denom = compute_log_likelihood(logbeta, logalpha)
+    return np.exp(log_nom-log_denom)
 
 # Joint Probability (p(qt,qt+1|u1,...,uT))
-def joint_prob(beta, alpha, U, mu, sigma):
-
-    X = alpha
-    Y = beta
-    (n,K) = X.shape
-
+def compute_joint_prob(logbeta, logalpha, U, mu, sigma):
+    (n,K) = logalpha.shape
     proba = np.zeros((n-1,K,K))
+    log_denom = compute_log_likelihood(logbeta, logalpha)
 
     for t in range(0,n-1):
         # calculate the log_denominator term with log-sum-exp trick
-        Z = X[t,:] + Y[t,:]
-        log_denom_t = np.max(Z) + logsumexp(Z-np.max(Z))
-
         log_prob_t = np.zeros((K,K))
-
-        # i loops over values taken by q(t+1)
-        for i in range(1,K):
+        # i loops over values taken by q_t+1
+        for i in range(K):
             mvnU = multivariate_normal.pdf(U[t+1,:], mean=mu[i,:], cov=sigma[i])
-            # j loops over values taken by q(t)
-            for j in range(1,K):
-                log_prob_t[i,j] = X[t,j] + Y[t+1,i] + np.log(A[i,j]) + np.log(mvnU) - log_denom_t
-
-        proba[t] = np.exp(log_prob_t)
+            # j loops over values taken by q_t
+            for j in range(K):
+                log_prob_t[i,j] = logalpha[t, j] + logbeta[t + 1, i] + np.log(A[i, j]) + np.log(mvnU)
+        proba[t] = np.exp(log_prob_t - log_denom)
 
     return proba
 
@@ -115,30 +107,24 @@ def joint_prob(beta, alpha, U, mu, sigma):
 # The initial parameters are given using the EM-gaussian algorithm
 def em_hmm(U, K, A, pis, mus, sigmas, max_iterations=1000, tolerance=0.0000000001):
     (T,d) = U.shape
-    old_expected_lklhood = -np.inf
+    old_log_likelihood = -np.inf
     for step in range(max_iterations):
         # E - step
-        alpha = logalpha(U,A,pis,mus,sigmas)
-        beta = logbeta(U, A, pis, mus, sigmas)
+        logalpha = compute_logalpha(U, A, pis, mus, sigmas)
+        logbeta = compute_logbeta(U, A, pis, mus, sigmas)
 
-        tau = smoothing(beta, alpha)
-        tau_transition = joint_prob(beta, alpha, U, mus, sigmas)
+        tau = smoothing(logbeta, logalpha)
+        tau_transition = compute_joint_prob(logbeta, logalpha, U, mus, sigmas)
         # At each t
         # the rows of tau_transition[t,:,:] are for q_t+1
         # the columns are for q_t
 
-        # Expected complete log-likelihood
-        expected_lklhood = np.sum(tau[0,:]*np.log(pis))
-        for t in range(T):
-            if t < T-1:
-                expected_lklhood += np.sum(tau_transition[t,:,:]*A)
-            for i in range(K):
-                mvnU = multivariate_normal.pdf(U[t, :], mean=mus[i, :], cov=sigmas[i])
-                expected_lklhood += tau[t,i]*np.log(mvnU)
-        print 'step ', step, ' ', expected_lklhood
-        if np.abs(expected_lklhood - old_expected_lklhood) < tolerance:
+        # log-likelihood
+        log_likelihood = compute_log_likelihood(logbeta, logalpha)
+        print 'step ', step, ' ', log_likelihood
+        if np.abs(log_likelihood - old_log_likelihood) < tolerance:
             break
-        old_expected_lklhood = expected_lklhood
+        old_log_likelihood = log_likelihood
 
         # M - step
         # We update pi
@@ -150,19 +136,20 @@ def em_hmm(U, K, A, pis, mus, sigmas, max_iterations=1000, tolerance=0.000000000
             for j in range(K): #j loops for q_t
                 A[i,j] = sum_tau_transition[i,j] / sum_tau[j]
             # We update mus
-            tau_u = np.dot(tau[:,i].T,U)
+            tau_u = np.dot(tau[:,i], U)
             mus[i,:] = tau_u / sum_tau[i]
             # We update sigmas
             U_centered = U - mus[i,:]
             sigmas[i] = np.zeros((d,d))
             for t in range(T):
-                covariance = np.dot(U_centered.T, U_centered)
+                Uc_t = U_centered[t,:].reshape((d,1))
+                covariance = np.dot(Uc_t, Uc_t.T)
                 sigmas[i] += tau[t,i]*covariance
             sigmas[i] = sigmas[i] / sum_tau[i]
 
-    return A, pis, mus, sigmas
+    return A, pis, mus, sigmas, tau
 
-
+################################ MAIN ##############################################
 
 # Plotting parameters
 plt.style.use("ggplot")
@@ -170,12 +157,12 @@ all_colors = [parameter['color'] for parameter in plt.rcParams['axes.prop_cycle'
 
 
 #import the data files
-data = np.loadtxt('data/EMGaussian.data')
+train = np.loadtxt('data/EMGaussian.data')
 test = np.loadtxt('data/EMGaussian.test')
 
 #center the data
-scaler = preprocessing.StandardScaler().fit(data)
-train = scaler.transform(data)
+scaler = preprocessing.StandardScaler().fit(train)
+train = scaler.transform(train)
 test = scaler.transform(test)
 
 U = train # npoints=500 x dim=2
@@ -191,14 +178,13 @@ T = 500
 epsilon = 1
 
 # compute alpha and beta for all test data .. after similar preprocessing to train data
-alpha = logalpha(V, A, pis, mus, sigmas)
-beta = logbeta(V, A, pis, mus, sigmas)
+logalpha = compute_logalpha(V, A, pis, mus, sigmas)
+logbeta = compute_logbeta(V, A, pis, mus, sigmas)
 
 # smoothing proba
-proba_smooting = smoothing(beta, alpha)
-
+proba_smooting = smoothing(logbeta, logalpha)
 # joint proba
-joint_proba = joint_prob(beta, alpha, U, mus, sigmas)
+joint_proba = compute_joint_prob(logbeta, logalpha, U, mus, sigmas)
 
 # plotting
 proba_smooting_sample = proba_smooting[:100,:]
@@ -213,7 +199,10 @@ proba_smooting_sample = proba_smooting[:100,:]
 # plt.suptitle("Smoothing: p(q_t|u_1,...,u_T) for 100 first values", size=24)
 # plt.show()
 
-A, pis, mus, sigmas = em_hmm(V,num_classes,A,pis,mus,sigmas)
+A, pis, mus, sigmas, tau = em_hmm(U,num_classes,A,pis,mus,sigmas)
+z_em = np.argmax(tau,1)
+plt.scatter(U[:, 0], U[:, 1], c=z_em)
+plt.show()
 
 #viterbi
 
